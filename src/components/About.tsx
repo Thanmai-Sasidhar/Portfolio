1a418089cbe410c2aec1e5b06ebb934c2b97fe7b
Avanigadda Thanmai Sasidhar <atsasidhar@gmail.com>
2025-09-13 18:03:39 +0530

    Update About.tsx

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -33,7 +33,7 @@ const About = () => {
           <div className="flex justify-center">
             <div className="w-80 h-80 overflow-hidden rounded-full shadow-lg border-r-4">
               <img
-                src="Me.jpeg"
+                src="/Me.jpeg"
                 alt="About Me"
                 className="w-full h-full object-cover"
               />
